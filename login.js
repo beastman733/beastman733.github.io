7874d331c3c00342d7e0979f3970195ff7bc4033
Beastman733 <beastman733@gmail.com>
2014-11-23 20:53:41 -0500

    AND ITS UP

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 <!-- Begin
 
 function Login(form) {
-username = new Array("u1","u2","u3","u4","u5","u6","u7","u8","u9","u10");
-password = new Array("p1","p2","p3","p4","p5","p6","p7","p8","p9","p10");
+username = new Array("beastman733","mywhitevan","awesomesauce5","jg","Emma","Izzy","Theo","u8","u9","u10");
+password = new Array("MONOCORNS","weezer","weatherbugs","christian","necktie","music","skate","p8","p9","p10");
 page = "hahahahahahahahahahahahahahaha" + "/";
 if (form.username.value == username[0] && form.password.value == password[0] || form.username.value == username[1] && form.password.value == password[1] || form.username.value == username[2] && form.password.value == password[2] || form.username.value == username[3] && form.password.value == password[3] || form.username.value == username[4] && form.password.value == password[4] || form.username.value == username[5] && form.password.value == password[5] || form.username.value == username[6] && form.password.value == password[6] || form.username.value == username[7] && form.password.value == password[7] || form.username.value == username[8] && form.password.value == password[8] || form.username.value == username[9] && form.password.value == password[9]) {
 self.location.href = page;
