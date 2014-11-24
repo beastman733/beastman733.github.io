a50b776d4eac0a17f8210e9990b938159bef8d0f
Beastman733 <beastman733@gmail.com>
2014-11-24 11:43:06 -0500

    kojjbj

--- login.js
+++ login.js
@@ -1,14 +1,14 @@
 <!-- Begin
 
 function Login(form) {
-username = new Array("beastman733","mywhitevan","awesomesauce5","jg","Emma","Izzy","Theo","u8","u9","u10");
+username = new Array("beastman733","mywhitevan","awesomesauce5","jg","emma","izzy","theo","u8","u9","u10");
 password = new Array("MONOCORNS","weezer","weatherbugs","christian","necktie","music","skate","p8","p9","p10");
 page = "hahahahahahahahahahahahahahaha" + "/";
 if (form.username.value == username[0] && form.password.value == password[0] || form.username.value == username[1] && form.password.value == password[1] || form.username.value == username[2] && form.password.value == password[2] || form.username.value == username[3] && form.password.value == password[3] || form.username.value == username[4] && form.password.value == password[4] || form.username.value == username[5] && form.password.value == password[5] || form.username.value == username[6] && form.password.value == password[6] || form.username.value == username[7] && form.password.value == password[7] || form.username.value == username[8] && form.password.value == password[8] || form.username.value == username[9] && form.password.value == password[9]) {
 self.location.href = page;
 }
 else {
-alert("Either the username or password you entered is incorrect.\nPlease try again.");
+alert("Go away Connor your not welcome here");
 form.username.focus();
 }
 return true;
